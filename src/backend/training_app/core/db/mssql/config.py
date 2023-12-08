df80dffbc362e9b16919bc85e695260c79d957f3
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2023-12-08 23:15:43 +0300

    created base settings models for db connect
    Changes to be committed:
    	modified:   src/backend/training_app/core/db/mssql/config.py

---
+++ src/backend/training_app/core/db/mssql/config.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+from .utils.string_connect_db_url import REAL_MSSQL_DATABASE_URL
+
+
+class DBSettings(BaseModel):
+    url: str = REAL_MSSQL_DATABASE_URL
+    # echo: bool = False
+    echo: bool = True
+
+
+class Settings(BaseSettings):
+    # api_v1_prefix: str = "/api/v1"
+
+    db: DBSettings = DBSettings()
+
+
+settings = Settings()
